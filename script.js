6ccb49e6ee9f50c2878ec28fe6bc04bc4fa9ac5f
Gladys Ndichu <itsgladysbrian@gmail.com>
2022-06-06 13:39:35 +0300

    put image, add background

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+h2.textContent = "Welcome to My Personal Website!";
 document.querySelector("body").appendChild(h2);
+
